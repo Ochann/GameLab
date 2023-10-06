f4689aa751d691ee4b9b70549178e4cd211380a9
Yuan Zhao <ec23858@qmul.ac.uk>
2023-10-06 10:40:31 +0100

    worked on lab2

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,15 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
     public Vector2 moveValue;
     public float speed = 500;
     private int count = 0;
+	private int numPickups = 5;
+	public TextMeshProUGUI scoreText;
+	public TextMeshProUGUI winText;
+	public TextMeshProUGUI playerPositionText;
+	public TextMeshProUGUI playerVelocityText;
+	
+	private void SetCountText(){
+		scoreText.text = "Score: " + count.ToString();
+		if(count >= numPickups){
+			winText.text = "You win!";
+		}
+	}
+	
+	private void SetPlayerPositionText(){
+		playerPositionText.text = "Player Position: " + transform.position.ToString("0.00");
+	}
+	
+	void Start(){
+		count = 0;
+		winText.text = "";
+		SetCountText();
+	}
 
     void OnMove (InputValue value) {
         moveValue = value.Get<Vector2>();
+		SetPlayerPositionText();
     }
 
     void FixedUpdate () {
@@ -23,6 +48,8 @@ public class PlayerController : MonoBehaviour
         if(other.gameObject.tag == "PickUp") {
             other.gameObject.SetActive(false);
             count += 1;
+			SetCountText();
         }
     }
+
 }
